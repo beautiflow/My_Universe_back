082bdf83a9c16d5761f1cd24785e61fbb47573c4
beautiflow <1114ny@naver.com>
2025-04-13 15:11:34 +0900

    cmt

--- obj/Debug/net9.0/VisitorCounter.AssemblyInfo.cs
+++ obj/Debug/net9.0/VisitorCounter.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("VisitorCounter")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+36ccfe222e6f7e96be1a1fcc60828b9f03b898a6")]
 [assembly: System.Reflection.AssemblyProductAttribute("VisitorCounter")]
 [assembly: System.Reflection.AssemblyTitleAttribute("VisitorCounter")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
